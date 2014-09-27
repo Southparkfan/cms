812c794f6d2b683dbd710cfe300ff2593fa53d17
Southparkfan <southparkfan223@hotmail.com>
2014-09-27 17:10:04 +0200

    Add isset - should fix issue #3 partially (only the undefined indexes are fixed)

--- login.php
+++ login.php
@@ -22,8 +22,8 @@ mysqlConnect();
 </table>
 </form>
 <?php
-$username = mysql_real_escape_string($_POST['username']);
-$password = mysql_real_escape_string($_POST['password']);
+$username = isset($_POST['username']) ? mysql_real_escape_string($_POST['username']) : '';
+$password = isset($_POST['password']) ? mysql_real_escape_string($_POST['password']) : '';
 $mysql_query = "SELECT * FROM user WHERE user_username = '$password' AND user_password = '$password';";
 mysqlQuery($mysql_query);
 if (mysql_num_rows($mysql_query) == 1) {
