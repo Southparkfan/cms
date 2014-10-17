cec974611827be4ce781753f8b0a0ccd75767db6
Southparkfan <Southparkfan@users.noreply.github.com>
2014-10-17 21:57:14 +0200

    Create Travis-CI setup

---
+++ .travis.phplint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author Addshore
+#https://github.com/Orain/ansible-playbook/blob/master/.travis.phplint.sh
+find ../ -type f -regex '.*\.php\|.*\.php\.j2' -exec php -l {} \; | grep "Errors parsing ";
+#Flip the exit code
+if [ $? -ne 0 ]
+then
+	exit 0
+else
+	exit 1
+fi
