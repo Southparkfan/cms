e14f18296ac339d253418007528da3d7f6c48b49
Southparkfan <Southparkfan@users.noreply.github.com>
2014-11-03 22:23:54 +0100

    Make a start on migrating functions to classes

---
+++ inc/DatabaseMySQL.php
@@ -0,0 +1,27 @@
+<?php
+class db {
+
+  private $DBserver;
+  private $DBuser;
+  private $DBpassword;
+  private $DBname;
+  private $query;
+  
+  function __construct($DBserver, $DBuser, $DBpassword, $DBname) {
+    $this->DBserver = $DBserver;
+    $this->DBuser = $DBuser;
+    $this->DBpassword = $DBpassword;
+    $this->$DBname = $DBname;
+  }
+  
+  public function connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname) {
+    $DBconnection = mysql_connect($this->DBserver, $this->DBuser, $this->DBpassword) or die(mysql_error());
+    mysql_select_db($this->DBname, $DBconnection) or die(mysql_error());
+  }
+  
+  public function query($query) {
+    global $DBconnection;
+    $this->query = mysql_query($query, $DBconnection);
+  }
+  
+}
