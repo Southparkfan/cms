73ef70ae31276df94722e57de6bb479dba56c6d5
Southparkfan <Southparkfan@users.noreply.github.com>
2014-11-06 18:20:27 +0100

    mysql_error -> mysqli_error

--- inc/DatabaseMySQL.php
+++ inc/DatabaseMySQL.php
@@ -8,19 +8,19 @@ class Database {
   public $query;
   
 	function __construct($DBserver, $DBuser, $DBpassword, $DBname) {
-    $this->DBserver = $DBserver;
-    $this->DBuser = $DBuser;
-    $this->DBpassword = $DBpassword;
-    $this->DBname = $DBname;
-	$this->connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname);
+	    $this->DBserver = $DBserver;
+	    $this->DBuser = $DBuser;
+	    $this->DBpassword = $DBpassword;
+	    $this->DBname = $DBname;
+	    $this->connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname);
 	}
   
 	function connect($DBserver, $DBuser, $DBpassword, $DBname) {
-    $this->DBconnection = mysqli_connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname) or die(mysql_error());
+    		$this->DBconnection = mysqli_connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname) or die(mysqli_error($this->DBconnection));
 	}
   
 	function query($query) {
-    $this->query = mysqli_query($this->DBconnection, $query) or die(mysql_error());
+    		$this->query = mysqli_query($this->DBconnection, $query) or die(mysqli_error($this->DBconnection));
 	}
   
 }
