c426046ece147e287a52c19208ee4a3094875671
Southparkfan <Southparkfan@users.noreply.github.com>
2014-11-06 17:40:20 +0100

    MySQL -> MySQLi, other fixes

--- inc/DatabaseMySQL.php
+++ inc/DatabaseMySQL.php
@@ -4,24 +4,23 @@ class Database {
   private $DBserver;
   private $DBuser;
   private $DBpassword;
-  private $DBname;
-  private $query;
+  public $DBname;
+  public $query;
   
-  function __construct($DBserver, $DBuser, $DBpassword, $DBname) {
+	function __construct($DBserver, $DBuser, $DBpassword, $DBname) {
     $this->DBserver = $DBserver;
     $this->DBuser = $DBuser;
     $this->DBpassword = $DBpassword;
     $this->DBname = $DBname;
-  }
+	$this->connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname);
+	}
   
-  public function connect($DBserver, $DBuser, DBpassword, $DBname) {
-    $DBconnection = mysql_connect($this->DBserver, $this->DBuser, $this->DBpassword) or die(mysql_error());
-    mysql_select_db($this->DBname, $DBconnection) or die(mysql_error());
-  }
+	function connect($DBserver, $DBuser, $DBpassword, $DBname) {
+    $this->DBconnection = mysqli_connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname) or die(mysql_error());
+	}
   
-  public function query($query) {
-    global $DBconnection;
-    $this->query = mysql_query($query, $DBconnection) or die(mysql_error());
-  }
+	function query($query) {
+    $this->query = mysqli_query($this->DBconnection, $query) or die(mysql_error());
+	}
   
 }
