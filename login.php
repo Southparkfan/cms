de99d13449423d69bd4b1acd8a048552995d161b
Southparkfan <Southparkfan@users.noreply.github.com>
2014-11-06 17:45:33 +0100

    Goodbye, mysqlConnect & mysqlQuery

--- login.php
+++ login.php
@@ -1,12 +1,10 @@
 <?php
 require 'functions.php';
-mysqlConnect();
 if (isset($_POST['poster'])) {
 $username = isset($_POST['username']) ? mysql_real_escape_string($_POST['username']) : '';
 $password = isset($_POST['password']) ? mysql_real_escape_string($_POST['password']) : '';
-$mysql_query = 'SELECT * FROM user WHERE user_username = ' . $username . ' AND user_password = ' . $password . ';';
-$mysql_query = mysqlQuery($mysql_query);
-if (mysql_num_rows($mysql_query) == 1) {
+$db->query('SELECT * FROM user WHERE user_username = ' . $username . ' AND user_password = ' . $password . ';');
+if (mysqli_num_rows($db->query) == 1) {
 	echo 'Successfully logged in! Do some session stuff here.';
 } else {
 	echo "<div style=\"background-color:#cc0000\";>\n";
