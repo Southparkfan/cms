96c0ea3036eb52031e61754c9c08edc397b16798
Southparkfan <Southparkfan@users.noreply.github.com>
2015-03-29 15:49:24 +0200

    fixes

--- login.php
+++ login.php
@@ -24,20 +24,20 @@ require 'functions.php';
 include 'header.php';
 
 if (isset($_POST['poster'])) {
-		$username = isset($_POST['username']) ? mysqli_real_escape_string($db->DBConnection, $_POST['username']) : '';
-		$password = isset($_POST['password']) ? mysqli_real_escape_string($db->DBConnection, $_POST['password']) : '';
+		$username = isset($_POST['username']) ? mysqli_real_escape_string($db->DBconnection, $_POST['username']) : '';
+		$password = isset($_POST['password']) ? mysqli_real_escape_string($db->DBconnection, $_POST['password']) : '';
 		$db->query("SELECT * FROM user WHERE user_username = '$username' AND user_password = '$password'");
 
-if (mysqli_num_rows($db->query) == 1) {
-	echo 'Successfully logged in!';
-	$dbrow = mysqli_fetch_assoc($db->query);
-	$_SESSION['userid'] = $row['user_id'];
-	$_SESSION['username'] = $row['user_name'];
-} else {
-	echo "<div style=\"background-color:#cc0000\";>\n";
-	echo "Username or password invalid!\n";
-	echo "</div>";
-}
+	if (mysqli_num_rows($db->query) == 1) {
+		echo 'Successfully logged in!';
+		$dbrow = mysqli_fetch_assoc($db->query);
+		$_SESSION['userid'] = $row['user_id'];
+		$_SESSION['username'] = $row['user_name'];
+	} else {
+		echo "<div style=\"background-color:#cc0000\";>\n";
+		echo "Username or password invalid!\n";
+		echo "</div>";
+	}
 }
 ?>
 <h1>Log in</h1>
