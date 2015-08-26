1bd6729e9e778192212f87529ae018d70d4292b8
Southparkfan <Southparkfan@users.noreply.github.com>
2015-08-26 22:38:52 +0200

    Give ScrutinizerCI some love

--- inc/DatabaseMySQL.php
+++ inc/DatabaseMySQL.php
@@ -25,22 +25,23 @@ class Database {
   private $DBserver;
   private $DBuser;
   private $DBpassword;
+  public $DBconnection;
   public $DBname;
   public $query;
-  
+
 	function __construct($DBserver, $DBuser, $DBpassword, $DBname) {
 	    $this->DBserver = $DBserver;
 	    $this->DBuser = $DBuser;
 	    $this->DBpassword = $DBpassword;
 	    $this->DBname = $DBname;
-	    $this->connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname);
+	    $this->connect();
 	}
   
-	function connect() {
+	public function connect() {
     		$this->DBconnection = mysqli_connect($this->DBserver, $this->DBuser, $this->DBpassword, $this->DBname) or die(mysqli_error($this->DBconnection));
 	}
   
-	function query($query) {
+	public function query($query) {
     		$this->query = mysqli_query($this->DBconnection, $query) or die(mysqli_error($this->DBconnection));
 	}
   
